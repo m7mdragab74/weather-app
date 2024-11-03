8b29265aac49a24a4650261822a45a765cb46ef5
m7mdragab <mr341349@gmail.com>
2024-11-03 17:08:32 +0200

    handle error in forecast

--- lib/cubits/weather cubit/weather_cubit.dart
+++ lib/cubits/weather cubit/weather_cubit.dart
@@ -1,20 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/weather%20cubit/weather_state.dart';
 import 'package:weather_app/services/current_weather_services.dart';
+import 'package:weather_app/services/forecast_weather_services.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit(this.weatherService) : super(WeatherInitial());
+  WeatherCubit(this.weatherService, this.forecastWeatherService)
+      : super(WeatherInitial());
 
   final CurrentWeatherServices weatherService;
+  final ForecastWeatherServices forecastWeatherService;
 
   void getWeather({required String cityName}) async {
     emit(WeatherLoading());
     try {
       final weatherModel =
           await weatherService.getWeatherData(cityName: cityName);
-      emit(WeatherSuccess(
-        weatherModel,
-      ));
+      final forecastList =
+          await forecastWeatherService.getForecastData(cityName: cityName);
+
+      emit(WeatherSuccess(weatherModel, forecastList));
     } catch (e) {
       emit(WeatherFailure('Failed to fetch current weather: $e'));
     }
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:weather_app/cubits/weather%20cubit/weather_cubit.dart';
 import 'package:weather_app/services/current_weather_services.dart';
 import 'package:weather_app/views/home_page.dart';
 
+import 'services/forecast_weather_services.dart';
+
 void main() {
   runApp(
     const WeatherApp(),
@@ -17,7 +19,10 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => WeatherCubit(CurrentWeatherServices(Dio())),
+      create: (context) => WeatherCubit(
+        CurrentWeatherServices(Dio()),
+        ForecastWeatherServices(Dio()),
+      ),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomeView(),
